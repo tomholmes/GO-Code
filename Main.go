fa474cb3bfa63f75a11338fbf2897ea62a986121
tomholmes <tom_holmes@mac.com>
2023-10-28 16:50:40 -0500

    Create Main.go
    Test Go code

---
+++ Main.go
@@ -0,0 +1,13 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "))
+}
